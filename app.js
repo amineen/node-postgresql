fbcaf520054add4211bb9abdd0d1f0f415a686f6
Aaron Mineen <amineen64@hotmail.com>
2022-01-04 00:29:37 +0000

    Setup cors and dotenv

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+require("dotenv");
+const cors = require("cors")
 
+const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res)=>{
+    res.status(200).json({
+        msg:"Welcome to API testing postgresql",
+        version:"0.0.1",
+        developer:"Aaron Mineen",
+        github:"https://github.com/amineen/node-postgresql"
+    })
+})
 
 PORT = process.env.PORT || 8080;
 
